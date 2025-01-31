9c4decbcda21182e1f3c0c00e86808720f113fb5
metetrkn <metetrkn52@gmail.com>
2025-01-31 09:47:42 +0100

    Some update in case wrong credentials

--- src/main/java/se/mete/springinloggning/controller/RoleBasedController.java
+++ src/main/java/se/mete/springinloggning/controller/RoleBasedController.java
@@ -1,7 +1,9 @@
 package se.mete.springinloggning.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Returns login and role-based views
@@ -15,7 +17,19 @@ public class RoleBasedController {
      * @return The name of the login view template (e.g., login.html)
      */
     @GetMapping("/login") // Maps HTTP GET requests to the /login endpoint
-    public String login() {
+    public String login(Model model, @RequestParam(value = "error", required = false) String error,
+                       @RequestParam(value = "logout", required = false) String logout) {
+
+        // If wrong credentials, gets this message on the screen
+        if (error != null) {
+            model.addAttribute("error", "Invalid username or password!");
+        }
+
+        // If logout successfully
+        if (logout != null) {
+            model.addAttribute("message", "You have been logged out successfully.");
+        }
+
         return "login"; // Returns the name of the login view template
     }
 
